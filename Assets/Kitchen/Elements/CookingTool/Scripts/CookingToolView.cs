811bf215cf9f24c1c1011da96658c7a843c38cb8
dialextremo <54782677+dialextremo@users.noreply.github.com>
2022-10-15 17:36:35 -0500

    Sonidos de UI iniciales

--- Assets/Kitchen/Elements/CookingTool/Scripts/CookingToolView.cs
+++ Assets/Kitchen/Elements/CookingTool/Scripts/CookingToolView.cs
@@ -9,6 +9,9 @@ namespace Kitchen
         private IngredientView currentlyCookingIngredient;
         private float currentlyCookingSeconds;
 
+        FMOD.Studio.EventInstance CookingSound;
+        FMOD.Studio.EventInstance ComidaLista;
+
         protected override void OnClick()
         {
             IngredientView ingredientView = SelectionManager.selectedGameObject as IngredientView;
@@ -19,6 +22,11 @@ namespace Kitchen
 
             currentlyCookingIngredient = Instantiate(ingredientView, transform.position, transform.rotation, transform);
             currentlyCookingSeconds = 0;
+
+            CookingSound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Enciende Sarten");
+            CookingSound.start();
+
+
         }
 
         private void Update()
@@ -28,9 +36,18 @@ namespace Kitchen
 
             currentlyCookingSeconds += Time.deltaTime;
             if (currentlyCookingIngredient.State == IngredientState.Raw && currentlyCookingSeconds >= cookingToolData.cookingSeconds)
+            {
                 currentlyCookingIngredient.State = IngredientState.Cooked;
+                ComidaLista = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Comida Lista");
+                ComidaLista.start();
+            }
+                
             else if(currentlyCookingIngredient.State == IngredientState.Cooked && currentlyCookingSeconds >= cookingToolData.burningSeconds)
+            {
                 currentlyCookingIngredient.State = IngredientState.Burned;
+                FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Comida Quemada", transform.position);
+            }
+                
         }
     }
 }
