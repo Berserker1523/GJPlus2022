4952e9ef106e0ae9951d117d43543faa8d39ba37
JesusCarcamoJC <jesus.carcamo@jamcity.com>
2022-10-16 11:56:59 -0500

    Potion Recipients Spawner

--- Assets/Kitchen/Elements/LevelManager/Scripts/LevelData.cs
+++ Assets/Kitchen/Elements/LevelManager/Scripts/LevelData.cs
@@ -4,13 +4,17 @@ using System;
 
 namespace Kitchen
 {
-    [CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/ClientSpawnData", order = 3)]
-    public class ClientSpawnData : ScriptableObject
+    [CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 3)]
+    public class LevelData : ScriptableObject
     {
         public int level;
         public int clientNumber;
         public float minSpawnSeconds;
         public float maxSpawnSeconds;
+        public int minNumberOfPotionRecipients;
+        public int minNumberOfMortars;
+        public int minNumberOfStoves;
+        public int minNumberOfPainKillers;
         public List<IngredientList> levelRecipes;
     }
 
