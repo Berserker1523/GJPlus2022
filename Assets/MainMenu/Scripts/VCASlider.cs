dfb048613df5dfc3347efe296e55ec9800d3445b
dialextremo <54782677+dialextremo@users.noreply.github.com>
2022-10-16 00:00:23 -0500

    Se añad Settings en menu principal

---
+++ Assets/MainMenu/Scripts/VCASlider.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VCASlider : MonoBehaviour
+{
+    private FMOD.Studio.VCA VcaController;
+    public string vcaName;
+
+    Slider mySlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + vcaName);
+        mySlider = GetComponent<Slider>();
+    }
+
+    public void setVCAVolume(float volume)
+    {
+        VcaController.setVolume(volume);
+    }
+}
