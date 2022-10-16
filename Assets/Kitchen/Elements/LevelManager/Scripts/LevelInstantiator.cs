a0ac756b32d172bceeb12499378773d032515e86
JesusCarcamoJC <jesus.carcamo@jamcity.com>
2022-10-16 12:29:43 -0500

    Mortars Instantiator

--- Assets/Kitchen/Elements/LevelManager/Scripts/LevelInstantiator.cs
+++ Assets/Kitchen/Elements/LevelManager/Scripts/LevelInstantiator.cs
@@ -13,17 +13,33 @@ namespace Kitchen
         [SerializeField] private GameObject potionPrefab;
         [SerializeField] private List<Transform> potionsPositions;
 
+        [Header("Mortars")]
+        [SerializeField] private GameObject mortarPrefab;
+        [SerializeField] private List<Transform> mortarsPositions;
+
         private void Awake()
         {
             InstantiatePotions(levelData.minNumberOfPotionRecipients);
+            InstantiateMortars(levelData.minNumberOfMortars);
         }
 
-        private void InstantiatePotions(int numberOfPotions)
+        private void InstantiatePotions(int quantity)
         {
-            potionsPositions = potionsPositions.OrderBy(potionTransform => int.Parse(potionTransform.gameObject.name)).ToList();
+            potionsPositions = potionsPositions.OrderBy(transform => int.Parse(transform.gameObject.name)).ToList();
 
             for (int i = 0; i < potionsPositions.Count; i++)
-                Instantiate(i < numberOfPotions ? potionPrefab : potionLockPrefab, potionsPositions[i]);
+                Instantiate(i < quantity ? potionPrefab : potionLockPrefab, potionsPositions[i]);
+        }
+
+        private void InstantiateMortars(int quantity)
+        {
+            mortarsPositions = mortarsPositions.OrderBy(transform => int.Parse(transform.gameObject.name)).ToList();
+
+            for (int i = 0; i < mortarsPositions.Count; i++)
+            {
+                if (i < quantity)
+                    Instantiate(mortarPrefab, mortarsPositions[i]);
+            }
         }
     }
 }
