9a32883809696b9e16a63ec4bad1a0d0d0d338f6
JotaPe <jcamacho249@unab.edu.co>
2023-02-18 03:08:01 -0300

    Add water to shaker fmod event placeholder

--- Assets/Kitchen/Elements/LevelManager/Scripts/SoundsManager.cs
+++ Assets/Kitchen/Elements/LevelManager/Scripts/SoundsManager.cs
@@ -20,6 +20,7 @@ namespace Kitchen
         string foodReadyEvent = "event:/SFX/Cocina/Comida Lista";
         string foodBurntEvent = "event:/SFX/Cocina/Comida Quemada";
         string addIngredientEvent = "event:/SFX/Cocina/Infusión";
+        string addWaterEvent = "";
         string shakeIngredientsEvent = "";
         string shakerPoofEvent = "event:/SFX/Jugabilidad/Completa receta";
         string failedRecipeEvent = "";
@@ -58,6 +59,7 @@ namespace Kitchen
             Events.EventManager.AddListener(IngredientState.Cooked, PlayFoodReadySFX);
             Events.EventManager.AddListener(IngredientState.Burnt, PlayFoodBurntSFX);
             Events.EventManager.AddListener(PotionEvent.AddIngredient, PlayAddIngredientSFX);
+            Events.EventManager.AddListener(PotionEvent.AddWater, PlayAddWater);
             Events.EventManager.AddListener(PotionEvent.FailedRecipe, PlayFailedRecipeSFX);
             Events.EventManager.AddListener(CookingToolEvents.Hover, PlayHoverSFX);
             Events.EventManager.AddListener(TrashEvent.Throw, PlayThrowTrashSFX);
@@ -89,6 +91,7 @@ namespace Kitchen
         void PlayFoodReadySFX() => PlaySFX(foodReadyEvent);
         void PlayFoodBurntSFX() => PlaySFX(foodBurntEvent);
         void PlayAddIngredientSFX() => PlaySFX(addIngredientEvent);
+        void PlayAddWater() => PlaySFX(addWaterEvent);
         void PlayFailedRecipeSFX() => PlaySFX(failedRecipeEvent);
         void PlayHoverSFX() => PlaySFX(hoverEvent);
         void PlayThrowTrashSFX() => PlaySFX(throwTrashEvent);
