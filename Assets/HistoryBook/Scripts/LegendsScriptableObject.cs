84cc87faca50349fdd23a814f06a9907c0cc8373
JotaPe <jcamacho249@unab.edu.co>
2023-05-25 15:23:15 -0500

    [update] MythsDatabase

--- Assets/HistoryBook/Scripts/LegendsScriptableObject.cs
+++ Assets/HistoryBook/Scripts/LegendsScriptableObject.cs
@@ -1,14 +1,15 @@
 using Kitchen;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization;
 
 [CreateAssetMenu(fileName = "MythsDatabase", menuName = "ScriptableObjects/Book/MythsContainer")]
 public class LegendsScriptableObject : ScriptableObject
 {
-    [SerializeField] public Myth[] myths ;
+    [SerializeField] public Myth[] myths;
+    [SerializeField] public IndigenousCommunity[] indigenousCommunities;
+    [SerializeField] public Ingredient[] ingredients;
+    [SerializeField] public Place[] places;
 }
 
 [Serializable]
@@ -20,6 +21,32 @@ public class Myth
     [SerializeField] public LocalizedString description;
     [SerializeField] public LocalizedString mythP1;
     [SerializeField] public LocalizedString mythP2;
-    [SerializeField] public Sprite ingredientSprite;
+    [SerializeField] public LocalizedString mythP3;
+    [SerializeField] public Sprite mythSprite;
     [SerializeField] public IngredientName refIngredient;
+}
+
+[Serializable]
+public class IndigenousCommunity
+{
+    [SerializeField] public LocalizedString name;
+    [SerializeField] public Sprite indigenousSprite;
+    [SerializeField] public LocalizedString[] texts;
+}
+
+[Serializable]
+public class Ingredient
+{
+    [SerializeField] public string name;
+    [SerializeField] public IngredientName refIngredient;
+    [SerializeField] public Sprite ingredientSprite;
+    [SerializeField] public LocalizedString[] texts;
+}
+
+[Serializable]
+public class Place
+{
+    [SerializeField] public string name;
+    [SerializeField] public Sprite placeSprite;
+    [SerializeField] public LocalizedString[] texts;
 }
